9302365bb9f98197b7d266fe4d3b27d1a372879c
Shahaf Shuhamy <Shahaf761@gmail.omm>
2021-06-08 22:47:44 +0300

    add login and signup methods tryout

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const app = express();
 
+const USERS_COLLECTION = "users";
 const COLLECTION_NAME = "events";
 
 app.use(cors({origin: true}));
@@ -14,14 +15,14 @@ app.use(cors({origin: true}));
  */
 app.get('/', async (req, res) => {
     const snapshot = await admin.firestore().collection(COLLECTION_NAME).orderBy("date", "asc").get();
-    let holidays = [];
-    snapshot.forEach(doc => {
-        let id = doc.id;
-        let data = doc.data();
+        let holidays = [];
+        snapshot.forEach(doc => {
+            let id = doc.id;
+            let data = doc.data();
 
-        holidays.push({id, ...data});
-    });
-    res.status(200).send(JSON.stringify(holidays));
+            holidays.push({id, ...data});
+        });
+        res.status(200).send(JSON.stringify(holidays));
 });
 
 
@@ -64,6 +65,25 @@ app.delete("/:id", async (req, res) => {
     await admin.firestore().collection(COLLECTION_NAME).doc(req.params.id).delete();
     res.status(200).send(`item with the id ${req.params.id} was deleted`);
 });
+
+/**
+ * check if the user email is in the users and if the password is the same.
+ */
+app.get("/login", async (req, res) => {
+    const user = await admin.firestore.collection(USERS_COLLECTION).doc(req.params.email).get();
+    if (user.password === req.params.password) {
+        res.status(200).send();
+    } else {
+        res.status(203).send("wrong password / user name");
+    }
+})
+
+
+app.post("/signup", async (req, res) => {
+    const user = req.body;
+    await admin.firestore.collection(USERS_COLLECTION).add(user);
+    res.status(200).send();
+})
 
 exports.events = functions.https.onRequest(app);
 
